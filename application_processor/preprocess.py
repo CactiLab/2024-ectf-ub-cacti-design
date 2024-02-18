9c7f807b73773d6ace4c16c943da533e17e98a44
Gaoxiang Liu <gliu25@buffalo.edu>
2024-02-17 23:17:08 -0500

    ap aead key nonce

--- application_processor/preprocess.py
+++ application_processor/preprocess.py
@@ -9,7 +9,7 @@ def read_binary_file(path):
 def format_key_for_c_define(key):
     return ', '.join(f'0x{byte:02x}' for byte in key)
 
-def parse_and_modify_header(header_path, ap_priv_key, cp_pub_key, ap_hash_key, ap_hash_salt, ap_hash_pin, ap_hash_token):
+def parse_and_modify_header(header_path, ap_priv_key, cp_pub_key, ap_hash_key, ap_hash_salt, ap_hash_pin, ap_hash_token, aead_key, aead_nonce):
     with open(header_path, 'r') as file:
         lines = file.readlines()
 
@@ -44,6 +44,8 @@ def parse_and_modify_header(header_path, ap_priv_key, cp_pub_key, ap_hash_key, a
         file.write(f'#define AP_HASH_TOKEN {format_key_for_c_define(ap_hash_token)}\n')
         file.write(f'#define AP_HASH_KEY {format_key_for_c_define(ap_hash_key)}\n')
         file.write(f'#define AP_HASH_SALT {format_key_for_c_define(ap_hash_salt)}\n')
+        file.write(f'#define AEAD_KEY {format_key_for_c_define(aead_key)}\n')
+        file.write(f'#define AEAD_NONCE {format_key_for_c_define(aead_nonce)}\n')
         file.write(f'#endif')
         
     
@@ -58,6 +60,8 @@ def main():
     parser.add_argument("--hash-salt-file", type=Path, required=True)
     parser.add_argument("--hash-pin-file", type=Path, required=True)
     parser.add_argument("--hash-token-file", type=Path, required=True)
+    parser.add_argument("--aead-key-file", type=Path, required=True)
+    parser.add_argument("--aead-nonce-file", type=Path, required=True)
     args = parser.parse_args()
     
     if not args.header_file.exists():
@@ -87,6 +91,14 @@ def main():
     if not args.hash_token_file.exists():
         print(f"Hash for token file {args.hash_token_file} does not exist. Build the deployment package first.")
         sys.exit(1)
+
+    if not args.aead_key_file.exists():
+        print(f"AEAD key file {args.aead_key_file} does not exist. Build the deployment package first.")
+        sys.exit(1)
+
+    if not args.aead_nonce_file.exists():
+        print(f"AEAD nonce file {args.aead_nonce_file} does not exist. Build the deployment package first.")
+        sys.exit(1)
     
     priv_key = args.ap_priv_key_file.read_bytes()
     pub_key = args.cp_pub_key_file.read_bytes()
@@ -94,8 +106,10 @@ def main():
     hash_salt = args.hash_salt_file.read_bytes()
     hash_pin = args.hash_pin_file.read_bytes()
     hash_token = args.hash_token_file.read_bytes()
+    aead_key = args.aead_key_file.read_bytes()
+    aead_nonce = args.aead_nonce_file.read_bytes()
     
-    cp_ids, cp_cnt, ap_pin, ap_token = parse_and_modify_header(args.header_file, priv_key, pub_key, hash_key, hash_salt, hash_pin, hash_token)
+    cp_ids, cp_cnt, ap_pin, ap_token = parse_and_modify_header(args.header_file, priv_key, pub_key, hash_key, hash_salt, hash_pin, hash_token, aead_key, aead_nonce)
     
     if ap_pin and ap_token and cp_ids and cp_cnt:
         print(f"AP_PIN: {ap_pin}")
