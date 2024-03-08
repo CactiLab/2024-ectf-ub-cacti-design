57dc82a136d32eb11951d18504be7a12c38bde94
Gaoxiang Liu <gliu25@buffalo.edu>
2024-03-08 17:56:57 -0500

    flush

--- application_processor/src/host_messaging.c
+++ application_processor/src/host_messaging.c
@@ -29,8 +29,11 @@ void recv_input(const char *msg, char *buf) {
     print_debug(msg);
     fflush(0);
     print_ack();
+    fflush(stdin);
     fflush(0);
     gets(buf);
+    fflush(0);
+    fflush(stdin);
     
     puts("");
 }
