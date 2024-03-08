e745977f99feee1c276166fbcde7afe127699ccc
Gaoxiang Liu <gliu25@buffalo.edu>
2024-03-08 09:53:31 -0500

    cp if

--- component/src/component.c
+++ component/src/component.c
@@ -478,22 +478,21 @@ int secure_receive(uint8_t* buffer) {
     // calculate the msg signature and verify
     retrive_ap_pub_key();
 
-    CONDITION_NEQ_BRANCH(crypto_eddsa_check(receiving_buf , flash_status.ap_pub_key, general_buf, NONCE_SIZE + 2 + len), 0, ERR_VALUE);
-    // verification failed - msg
-    crypto_wipe(buffer, MAX_I2C_MESSAGE_LEN);
+    EXPR_EXECUTE(crypto_eddsa_check(receiving_buf , flash_status.ap_pub_key, general_buf, NONCE_SIZE + 2 + len), ERR_VALUE);
+    crypto_wipe(flash_status.ap_pub_key, sizeof(flash_status.ap_pub_key));
+    EXPR_CHECK(ERR_VALUE);
+    RANDOM_DELAY_TINY;
+    if (if_val_2 == 0) {
+        RANDOM_DELAY_TINY;
+            if (if_val_2 == 0) {
+                memcpy(buffer, receiving_buf + SIGNATURE_SIZE, len);
+                MXC_Delay(500);
+                return len;
+            }
+    }
+
     defense_mode();
     return 0;
-    CONDITION_BRANCH_ENDING(ERR_VALUE);
-    // verification passed - msg
-
-    // wipe
-    crypto_wipe(flash_status.ap_pub_key, sizeof(flash_status.ap_pub_key));
-
-    // save the plain message
-    memcpy(buffer, receiving_buf + SIGNATURE_SIZE, len);
-
-    MXC_Delay(500);
-    return len;
 }
 
 /******************************* FUNCTION DEFINITIONS *********************************/
